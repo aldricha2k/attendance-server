c95c41949ab4e29a1e2f3d4247cf2fcabd6ca0bf
aldricha2k <159392428+aldricha2k@users.noreply.github.com>
2024-02-08 10:57:54 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require("./models/User");
+require("./models/Attendance");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+const accountRoutes = require('./routes/accountRoutes');
+const attendanceRoutes = require('./routes/attendanceRoutes');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(accountRoutes);
+app.use(attendanceRoutes);
+
+const mongoUri = "mongodb+srv://Aldrich:passwordpassword@cluster0.6tkkc1r.mongodb.net/";
+
+
+mongoose.set("strictQuery", true);
+// resolves future deprecation issue with Mongoose v7
+
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to construction database");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
---
+++ src/models/Attendance.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const AttendanceSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    time_in: {
+        type: String,
+    },
+    time_out: {
+        type: String,
+        default: ''
+    },
+    date: {
+        type: String,
+    },
+    week: {
+        type: String,
+    },
+    week_counter: {
+        type: Number,
+    },
+    month_year: {
+        type: String,
+    },
+    absent: {
+        type: Boolean,
+    },
+    deduction: {
+        type: Number,
+        default: ''
+    }
+});
+
+mongoose.model('Attendance', AttendanceSchema);
