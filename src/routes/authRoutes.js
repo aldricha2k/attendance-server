32a83e0d7f4aa34c8e57217c9fbabceaf0d10132
aldricha2k <159392428+aldricha2k@users.noreply.github.com>
2024-02-12 11:02:53 +0800

    Add files via upload

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.get('/account', async (req, res) => {
+  try{
+    const details = await User.find({ userId: req.user.perscode });
+    res.send(details);  
+  }
+  catch (err) {
+    return res.status(422).send({ err });
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: 'Invalid email.' });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    const role = user.role;
+    const perscode = user.perscode;
+    const name = user.name;
+    res.send({ token, role, perscode, name });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password.' });
+  }
+});
+
+module.exports = router;
