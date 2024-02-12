84c7da69b285e4309da071d32a60c384cfa4fbb5
aldricha2k <159392428+aldricha2k@users.noreply.github.com>
2024-02-12 09:15:05 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 require("./models/User");
-require("./models/Attendance");
 
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const authRoutes = require("./routes/authRoutes");
 const requireAuth = require("./middlewares/requireAuth");
-const accountRoutes = require('./routes/accountRoutes');
-const attendanceRoutes = require('./routes/attendanceRoutes');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
-app.use(accountRoutes);
-app.use(attendanceRoutes);
 
-const mongoUri = "mongodb+srv://Aldrich:passwordpassword@cluster0.6tkkc1r.mongodb.net/";
-
+const mongoUri = 'mongodb+srv://Aldrich:passwordpassword@cluster0.6tkkc1r.mongodb.net/';
 
 mongoose.set("strictQuery", true);
-// resolves future deprecation issue with Mongoose v7
 
 mongoose.connect(mongoUri);
 mongoose.connection.on("connected", () => {
-  console.log("Connected to construction database");
+  console.log("Connected to attendance database");
 });
 mongoose.connection.on("error", (err) => {
   console.error("Error connecting to mongo", err);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,42 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+
 const User = mongoose.model('User');
 
 const router = express.Router();
-
-router.post('/signup', async (req, res) => {
-  const {
-    name,
-    perscode,
-    email,
-    password,
-    division,
-    role
-    } = req.body;
-
-  try {
-    const user = new User({
-      name,
-      perscode,
-      email,
-      password,
-      division,
-      role 
-    });
-    await user.save();
-
-    const signinToken = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-    res.send({ token: signinToken });
-  } catch (err) {
-    return res.status(422).send(err.message);
-  }
-});
-router.get('/signin', async (req, res) => {
-  const response = await User.find();
-
-  res.send(response);
-});
 
 router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
@@ -47,18 +15,15 @@ router.post('/signin', async (req, res) => {
 
   const user = await User.findOne({ email });
   if (!user) {
-    return res.status(422).send({ error: 'Invalid password or email' });
+    return res.status(422).send({ error: 'Invalid email.' });
   }
 
   try {
     await user.comparePassword(password);
-    const signinToken = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-    const role = user.role;
-    const _id = user._id;
-    const name = user.name;
-    res.send({ token: signinToken, role, _id, name });
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
   } catch (err) {
-    return res.status(422).send({ error: 'Invalid password or email' });
+    return res.status(422).send({ error: 'Invalid password.' });
   }
 });
 
