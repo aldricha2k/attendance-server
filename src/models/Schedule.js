991a3b8d90c4b275c108ef09acaf0efd3ba5f040
aldricha2k <159392428+aldricha2k@users.noreply.github.com>
2024-04-01 10:13:32 +0800

    Add files via upload

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const scheduleSchema = new mongoose.Schema({
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            default: mongoose.Types.ObjectId
-        },
         title: {
             type: String,
         },
